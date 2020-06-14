db05fd16f731783befe134c9c539407fa2e094b2
Vladislav Brezgun <nextydev@gmail.com>
2020-06-14 20:59:10 +0300

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using SignalRTestApp.Models;
+
+namespace SignalRTestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        IHubContext<ChatHub> hubContext;
+        public HomeController(IHubContext<ChatHub> hubContext)
+        {
+            this.hubContext = hubContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task Create(string connectionId, string user, string message)
+        {
+            Message msg1 = new Message()
+            {
+                UserName = user,
+                Text = message,
+                Class = "text-dark"
+            };
+            Message msg2 = new Message()
+            {
+                UserName = user,
+                Text = message,
+                Class = "text-light bg-dark"
+            };
+
+            await hubContext.Clients.AllExcept(connectionId).SendAsync("Notify", msg1);
+            await hubContext.Clients.Client(connectionId).SendAsync("Notify", msg2);
+        }
+    }
+}
---
+++ Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRTestApp.Models
+{
+	public class Message
+	{
+		public string UserName { get; set; }
+		public string Text { get; set; }
+		public string Class { get; set; }
+	}
+}
